59fe6ac951d32bfec075bf96aea99fb321da40e5
yamd1 <w5000c2d26@gmail.com>
2024-01-03 21:33:28 +0900

    :sparkles: 初期化時にCommitIDをオプショナルで渡せるように

--- src/domain/git.rs
+++ src/domain/git.rs
@@ -1,19 +1,27 @@
 use super::commit::Commit;
 
 pub struct Git {
+    last_commit_id: u32,
     name: String,
 }
 
 impl Git {
-    pub fn new(name: String) -> Self {
-        Git { name }
+    pub fn new(id: Option<u32>, name: String) -> Self {
+        let last_commit_id = match id {
+            None => 0,
+            Some(v) => v,
+        };
+        Git {
+            last_commit_id,
+            name,
+        }
     }
 
     pub fn commit(&self, message: String) -> Commit {
         Commit::new(message)
     }
 
-    pub fn echo(&self) -> &str {
-        &self.name
+    pub fn echo(&self) {
+        println!("{}, {}", &self.last_commit_id, &self.name)
     }
 }
