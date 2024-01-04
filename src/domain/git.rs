05d9b074fc9db0f982c8f7c778ef5703b3ae4063
yamd1 <w5000c2d26@gmail.com>
2024-01-04 22:06:06 +0900

    ✨ logメソッドとテストを追加

--- src/domain/git.rs
+++ src/domain/git.rs
@@ -7,11 +7,8 @@ pub struct Git {
 }
 
 impl Git {
-    pub fn new(id: Option<u32>, name: String) -> Self {
-        let last_commit_id = match id {
-            None => 0,
-            Some(v) => v,
-        };
+    pub fn new(name: String) -> Self {
+        let last_commit_id = 0;
         Git {
             last_commit_id,
             name,
@@ -23,7 +20,30 @@ impl Git {
         Commit::new(self.last_commit_id, message)
     }
 
+    pub fn log(&self) -> Vec<Git> {
+        let history: Vec<Git> = Vec::new();
+
+        history
+    }
+
     pub fn echo(&self) {
         println!("{}, {}", self.last_commit_id, self.name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn success() {
+        let mut repo = Git::new("test".to_string());
+        repo.commit("first commit".to_string());
+        repo.commit("second commit".to_string());
+
+        let logs = repo.log();
+        assert_eq!(logs.len(), 2);
+        assert_eq!(logs[0].last_commit_id, 0);
+        assert_eq!(logs[1].last_commit_id, 1);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod domain;
 use domain::git::Git;
 
 fn main() {
-    let mut git = Git::new(None, "own-git".to_string());
+    let mut git = Git::new("own-git".to_string());
     git.echo();
-    let mut commit = git.commit("hello".to_string());
+    let commit = git.commit("hello".to_string());
     commit.echo_message();
 
-    let mut commit = git.commit("second commit".to_string());
+    let commit = git.commit("second commit".to_string());
     commit.echo_message();
 }
